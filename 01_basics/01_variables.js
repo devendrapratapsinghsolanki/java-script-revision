d477de0493f78b8b6fedf1e354028558886ea3e3
DEVENDRA PRATAP SINGH SOLANKI <dt9761233@gmail.com>
2026-05-30 21:19:04 +0000

    variables are clear

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,21 @@
+const accountId = 1444533
+let accountEmail = "dev@gmail.com"
+var accountPass = "1234"
+accountCity = "Jaipur"
+
+
+// accountId = 870909 we can not do this
+
+accountEmail = "ram@gmail.com"
+accountPass = "122222"
+accountCity  = "Rampur"
+
+
+console.log(accountId);
+console.log(accountEmail);
+console.log(accountPass);
+console.log(accountCity);
+
+
+
+
