27db31f86300c9b0ac4fc80fe1af6d0519e64c2a
DEVENDRA PRATAP SINGH SOLANKI <dt9761233@gmail.com>
2026-06-03 19:24:19 +0000

    complete the revision of math function

--- 01_basics/06-nums_and_maths.js
+++ 01_basics/06-nums_and_maths.js
@@ -17,5 +17,35 @@ const hunderds = 10000000;
 console.log(hunderds.toLocaleString('en-In'));
 
 
+/*
+Math -> most of the use of math library in random function
+*/
+
+console.log(Math.abs(-4)); //output -> 4
+console.log(Math.abs(4)); //output -> 4
+
+console.log(Math.round(4.88)); //output -> 5
+console.log(Math.ceil(4.1)); //output -> 5
+console.log(Math.floor(4.9)); //output -> 4
+
+
+console.log(Math.random());
+console.log(Math.floor((Math.random()*10)+1));
+
+
+const min = 20;
+const max = 30;
+
+console.log
+(Math.floor((Math.random()* (max - min + 1))+min));
+
+
+
+
+
+
+
+
+
 
 
