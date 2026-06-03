af999da3dba3ff316f8a42daaa75b1963aa0fa19
DEVENDRA PRATAP SINGH SOLANKI <dt9761233@gmail.com>
2026-06-03 19:04:29 +0000

    complete the revision of numbers

---
+++ 01_basics/06-nums_and_maths.js
@@ -0,0 +1,21 @@
+const score = 400
+console.log(score);
+
+
+const balance = new Number(400)
+console.log(balance);
+
+console.log(balance.toString().length);
+console.log(balance.toFixed(2));
+
+
+const anotherNumber = 123.823;
+console.log(anotherNumber.toPrecision(2));
+
+
+const hunderds = 10000000;
+console.log(hunderds.toLocaleString('en-In'));
+
+
+
+
