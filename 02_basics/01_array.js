9b0047e43342a18745a158e303aa85d5fce8ff01
DEVENDRA PRATAP SINGH SOLANKI <dt9761233@gmail.com>
2026-06-04 18:22:37 +0000

    array

---
+++ 02_basics/01_array.js
@@ -0,0 +1,14 @@
+const array1 = [1,2,3,4];
+const array2 = ["devendra", "ram", "raju"];
+
+const myarr = new Array(11,22,33, "hitesh");
+console.log(array1);
+console.log(array2);
+
+console.log(myarr);
+
+console.log(myarr.length);
+
+// Arary method
+
+
