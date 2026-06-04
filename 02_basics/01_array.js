c364efb38ad481b8d3d237127ac169da9fa9161b
DEVENDRA PRATAP SINGH SOLANKI <dt9761233@gmail.com>
2026-06-04 18:28:48 +0000

    jj

--- 02_basics/01_array.js
+++ 02_basics/01_array.js
@@ -5,10 +5,24 @@ const myarr = new Array(11,22,33, "hitesh");
 console.log(array1);
 console.log(array2);
 
-console.log(myarr);
 
 console.log(myarr.length);
 
 // Arary method
+
+myarr.push(77,44);
+
+console.log(myarr);
+myarr.pop();
+console.log(myarr);
+console.log(myarr.unshift(3));
+
+
+let araray = [1,2,3];
+araray.unshift(0);
+araray.shift()
+console.log(araray);
+
+
 
 
