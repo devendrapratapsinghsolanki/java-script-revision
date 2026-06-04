66715a6bd23cd982214c6bd6b91c6c07cf3881f2
DEVENDRA PRATAP SINGH SOLANKI <dt9761233@gmail.com>
2026-06-04 18:47:08 +0000

    some basics are cleard of array object

--- 02_basics/01_array.js
+++ 02_basics/01_array.js
@@ -1,27 +1,61 @@
-const array1 = [1,2,3,4];
-const array2 = ["devendra", "ram", "raju"];
+// const array1 = [1,2,3,4];
+// const array2 = ["devendra", "ram", "raju"];
 
-const myarr = new Array(11,22,33, "hitesh");
-console.log(array1);
-console.log(array2);
-
-
-console.log(myarr.length);
-
-// Arary method
-
-myarr.push(77,44);
-
-console.log(myarr);
-myarr.pop();
-console.log(myarr);
-console.log(myarr.unshift(3));
+// const myarr = new Array(11,22,33, "hitesh");
+// console.log(array1);
+// console.log(array2);
 
 
-let araray = [1,2,3];
-araray.unshift(0);
-araray.shift()
-console.log(araray);
+// console.log(myarr.length);
+
+// // Arary method
+
+// myarr.push(77,44);
+
+// console.log(myarr);
+// myarr.pop();
+// console.log(myarr);
+// console.log(myarr.unshift(3));
+
+
+// let araray = [1,2,3];
+// araray.unshift(0);
+// araray.shift()
+// console.log(araray);
+
+let newArray = new Array(1,2,3,4,5,6,7,8);
+// console.log(newArray.includes(5));
+// console.log(newArray.indexOf(8));
+
+const array1 = newArray.join();
+// console.log(newArray);
+// console.log(typeof newArray);
+
+// console.log(array1);
+// console.log(typeof array1);
+
+// slice and splice method
+console.log("A", newArray);
+let myn1 = newArray.slice(1,4);
+console.log(myn1);
+
+console.log("B", newArray);
+
+let  myn2 = newArray.splice(1,4);
+console.log(myn2);
+
+console.log("C", newArray);
+
+
+
+
+
+
+
+
+
+
+
 
 
 
