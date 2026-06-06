c6866a546e12bc615fe575ee315bc747b7a5d686
DEVENDRA PRATAP SINGH SOLANKI <dt9761233@gmail.com>
2026-06-06 07:02:51 +0000

    changes

--- 02_basics/04_objects.js
+++ 02_basics/04_objects.js
@@ -6,6 +6,11 @@ tinder.use_age = 32;
 tinder.user_proffesion = "Software Engineer";
 tinder.userIsLogin = false;
 
+// console.log(Object.keys(tinder));
+// console.log(Object.values(tinder));
+console.log(tinder.hasOwnProperty('id'))
+
+
 
 
 const regularUser = {
@@ -29,4 +34,31 @@ const obj2 = {3:"c", 4:"d"}
 
 
 const obj3 = Object.assign({}, obj1, obj2);
-console.log(obj3);
+// console.log(obj3);
+
+
+const user = [
+    {
+        id : 1,
+        name : "dev",
+        email : "dev@gmial.com"
+    },
+      {
+        id : 2,
+        name : "ram",
+        email : "ram@gmial.com"
+    },
+      {
+        id : 3,
+        name : "john",
+        email : "john@gmial.com"
+    },
+      {
+        id : 4,
+        name : "suresh",
+        email : "suresh@gmial.com"
+    }
+ 
+]
+
+// console.log(user[0].email);
