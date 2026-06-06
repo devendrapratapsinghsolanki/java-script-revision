9db7d0e37e266a52517c2fd32ddfc022ab5cca4c
DEVENDRA PRATAP SINGH SOLANKI <dt9761233@gmail.com>
2026-06-06 19:29:13 +0000

    function is started

---
+++ 03_basics/01_functions.js
@@ -0,0 +1,11 @@
+function addTwoNumbers(num1, num2) {
+    return num1 + num2
+}
+
+// console.log(addTwoNumbers(7,7))
+
+function userLoggedinMessage(user) {
+    return `${user} just logged in`
+}
+
+   console.log(userLoggedinMessage("Devendra"))
