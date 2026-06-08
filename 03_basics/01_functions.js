211eccaf0d1617a203f82a77845698d55509f0d5
DEVENDRA PRATAP SINGH SOLANKI <dt9761233@gmail.com>
2026-06-08 18:17:07 +0000

    function handling

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -8,4 +8,23 @@ function userLoggedinMessage(user) {
     return `${user} just logged in`
 }
 
-   console.log(userLoggedinMessage("Devendra"))
+//    console.log(userLoggedinMessage("Devendra"))
+
+function calculateCartPrice (val,val2, ...num){
+    return num
+}
+
+console.log
+(calculateCartPrice(100,200,300,400));
+
+const users = {
+    name: "Deve",
+    price: 199
+}
+
+function handleObj(anyobject){
+    console.log(`User Name is ${anyobject.name} and price is ${anyobject.price}`);
+    
+}
+
+handleObj(users);
