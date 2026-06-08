f0bc9d1bb52edcb969fcbeb27eaa86a9e011ba93
DEVENDRA PRATAP SINGH SOLANKI <dt9761233@gmail.com>
2026-06-08 18:23:19 +0000

    function handling

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -14,17 +14,25 @@ function calculateCartPrice (val,val2, ...num){
     return num
 }
 
-console.log
-(calculateCartPrice(100,200,300,400));
+// console.log
+// (calculateCartPrice(100,200,300,400));
 
-const users = {
-    name: "Deve",
-    price: 199
-}
+
 
 function handleObj(anyobject){
     console.log(`User Name is ${anyobject.name} and price is ${anyobject.price}`);
     
 }
 
-handleObj(users);
+
+handleObj({
+    name: "Deve",
+    price: 139
+})
+
+function returnSecondValue (getArray){
+    return getArray[1]
+
+}
+
+console.log(returnSecondValue([1,2,3,4,5]))
