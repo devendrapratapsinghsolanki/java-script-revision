39ba9af80512fae1d50ca585ba3fe37a7e655330
DEVENDRA PRATAP SINGH SOLANKI <dt9761233@gmail.com>
2026-06-19 17:58:17 +0000

    scope

---
+++ 03_basics/02_scope.js
@@ -0,0 +1,27 @@
+let a = 900;
+
+if (true){
+    let a = 10;
+    const b = 20;
+    var c = 30;
+
+
+}
+
+function one(){
+    const name = "Devendra"
+
+    function two (){
+        const website = "devendrapratap.in"
+
+        console.log(name);
+        console.log(website);
+        
+        
+    }
+
+    two()
+
+}
+
+one()
