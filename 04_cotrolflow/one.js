4bbdce4d835e08dd50daf7a855c921d90c82d4d3
DEVENDRA PRATAP SINGH SOLANKI <dt9761233@gmail.com>
2026-06-20 12:53:49 +0000

    Now I am moving on Control or logic flow of js

---
+++ 04_cotrolflow/one.js
@@ -0,0 +1,19 @@
+const temp= 40;
+
+if (temp <50) {
+    console.log("It's cold outside");
+} else if (temp < 70) {
+    console.log("It's warm outside");
+} else {
+    console.log("It's hot outside");
+}
+
+
+const score = 85;
+
+if (score > 80){
+    const power = 2;
+    console.log(`You passed with a score of ${score} and your power is ${power}`);
+}
+
+// console.log(`Your score is ${power}`); // This will cause an error because power is not defined outside the if block
