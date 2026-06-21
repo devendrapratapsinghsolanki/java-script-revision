5ec7175cba9155e560b27c0f2b97fc7ac23ff58c
DEVENDRA PRATAP SINGH SOLANKI <dt9761233@gmail.com>
2026-06-21 18:29:35 +0000

    chanes

---
+++ iterations/five.js
@@ -0,0 +1,13 @@
+const arr = ["js", "py", "java", "cpp"];
+
+// arr.forEach( function (val){
+//     console.log(`value: ${val}`);
+// })
+
+// arr.forEach( (item) => {
+//     console.log(`item: ${item}`);
+// })
+
+// arr.forEach((item, index, array) =>{
+//     console.log(`item: ${item}, index: ${index}, array: ${array}`);
+// })
--- iterations/four.js
+++ iterations/four.js
@@ -8,3 +8,12 @@ const myObject= {
 for(const key in myObject){
     console.log(`key: ${key}, value: ${myObject[key]}`);
 }
+
+// for in loop is used to iterate over the properties of an object. It returns the keys of the object, which can be used to access the values of the object.
+
+const promgrammingLanguages = ['JavaScript', 'Python', 'Java', 'C++'];
+
+
+for(const index in promgrammingLanguages){
+    console.log(`index: ${index}, value: ${promgrammingLanguages[index]}`);
+}
